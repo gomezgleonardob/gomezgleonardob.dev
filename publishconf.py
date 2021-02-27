cec513d0bded1e308193cc0b06ae54189ed1e560
gomezgleonardob <gomezgleonardob@riseup.net>
2021-02-27 01:49:37 -0500

    update dir website

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://gomezgleonardob.dev/'
+SITEURL = 'https://gomezgleonardob.dev'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
